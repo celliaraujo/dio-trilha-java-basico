eb998d1877e79f5e2dc8246549ded03d7ab6772f
celliaraujo <celliaraujo@gmail.com>
2023-02-23 16:40:53 -0300

    Tipos variaveis final

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá");
+
+        //tipos primitivos
+        //estudar classe String que representa textos na aplicação
+
+        String meuNome = "Marcelli Araujo";
+        System.out.println(meuNome);
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
