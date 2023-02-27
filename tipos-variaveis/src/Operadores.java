acbb1d176cd93bb60c07ae7001bb765ba8c47199
celliaraujo <celliaraujo@gmail.com>
2023-02-27 09:56:23 -0300

    Alteração final

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {    
+    public static void main(String[] args) {
+
+        String nomeUm = "MARCELLI";
+        String nomeDois = new String("MARCELLI");
+
+        System.out.println(nomeUm.equals(nomeDois));
+        
+    }
+}
